f9fa35e38ed180dac5c307a8843337ff592e6193
Namyejun <121931660+Namyejun@users.noreply.github.com>
2024-09-10 23:42:08 +0900

    Update SERVER.js

--- frontend/src/util/SERVER.js
+++ frontend/src/util/SERVER.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const SERVER = axios.create({
-  baseURL: "http://3.38.95.48",
+  baseURL: "http://13.210.155.222",
   headers: {
     "Content-Type": "application/json",
   }
